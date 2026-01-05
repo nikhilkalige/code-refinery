06661e6c9bb28229e00c38d896695b4f0d346ccf
Nikhil Kalige <Nikhil.Kalige@intusurg.com>
2026-01-05 15:49:00 -0800

    Euler 114

--- euler/114/solution.py
+++ euler/114/solution.py
@@ -7,10 +7,10 @@ URL: https://projecteuler.net/problem=114
 import sys
 
 
-def solve(N: int, min_len: int) -> int:
+def solve(block_size: int) -> int:
     """
     Calculates the number of ways to fill a row of length N with blocks
-    of minimum length 'min_len', separated by at least one gap.
+    of minimum length 'block_size', separated by at least one gap.
 
     Logic:
     To find the total ways for a row of length 'n' (dp[n]), we look at
@@ -21,34 +21,33 @@ def solve(N: int, min_len: int) -> int:
        - Ways: dp[n-1]
 
     2. It starts with a RED block of length 'k':
-       - 'k' must be between min_len and n.
+       - 'k' must be between block_size and n.
        - If k == n: The whole row is red (1 way).
        - If k < n: The block MUST be followed by a grey spacer.
          This uses (k + 1) squares total.
          Ways: dp[n - (k + 1)]
     """
-    # dp[i] stores the number of ways to fill a row of length i
-    dp = [0] * (N + 1)
+    # We don't know the value of N, lets start with a 1000
+    N = 1000
+    C = 1_000_000
 
-    # Base Case: An empty row has 1 way to be "filled" (doing nothing)
+    dp = [0] * (N + 1)
     dp[0] = 1
 
-    # Build the DP table from the bottom up
     for n in range(1, N + 1):
-        # Case 1: First square is grey
         dp[n] = dp[n - 1]
 
-        # Case 2: First block is red of length 'bsize'
-        for bsize in range(min_len, n + 1):
-            if bsize == n:
-                dp[n] += 1  # The entire row is one solid red block
+        for m in range(block_size, n + 1):
+            if m == n:
+                dp[n] += 1
             else:
-                # Red block (bsize) + Spacer (1) leaves (n - bsize - 1) squares
-                dp[n] += dp[n - bsize - 1]
+                dp[n] += dp[n - (m + 1)]
 
-    return dp[N]
+        if dp[n] > C:
+            return n
+    return N
 
 
 if __name__ == "__main__":
-    [a, b] = [int(n) for n in sys.stdin.readline().split(" ")]
-    print(solve(a, b))
+    [a] = [int(n) for n in sys.stdin.readline().split(" ")]
+    print(solve(a))
